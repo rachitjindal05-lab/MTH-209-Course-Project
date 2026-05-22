009f1557a005246a5f63a43fc9639b457475f345
Rachitj24 <rachitjindal05@gmail.com>
2026-05-22 15:28:44 +0530

    Add files via upload

---
+++ runme.R
@@ -0,0 +1,8 @@
+# 1. Install the package directly from bundled file
+install.packages("gradR_1.0.0.tar.gz", repos = NULL, type = "source")
+
+# 2. Load the library officially
+library(gradR)
+
+# 3. Launch the app
+visualize_gradientdescent()
